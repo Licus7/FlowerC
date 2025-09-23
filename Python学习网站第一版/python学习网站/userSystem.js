5f4dd8240ee2a7d27c19ce838117803f570a4e2b
Licus7 <a18731258515@163.com>
2025-09-23 22:26:11 +0800

    userSystem链接

--- Python学习网站第一版/python学习网站/userSystem.js
+++ Python学习网站第一版/python学习网站/userSystem.js
@@ -302,6 +302,8 @@ class UserSystem {
             document.body.appendChild(userInfoDiv);
             
             // 添加事件监听111111111111111111111111111111111111111111111111111111111111
+            //11111111111111111121212121212121212121212121212121212121212121212121212
+            //112121212121231321321423142351423143254132541352415243154231432514325142143254123541325143251
             document.getElementById('continueBtn').addEventListener('click', () => {
                 window.location.href = 'menu.html';
             });
